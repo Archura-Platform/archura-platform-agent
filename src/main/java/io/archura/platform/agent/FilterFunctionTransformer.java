f1ea06ef8ed755b1b2795121c009487adfc1f6f1
Can MOGOL <canmogol@gmail.com>
2022-09-14 20:21:20 +0200

    Handle Sockets.

--- src/main/java/io/archura/platform/agent/FilterFunctionTransformer.java
+++ src/main/java/io/archura/platform/agent/FilterFunctionTransformer.java
@@ -1,20 +1,16 @@
 package io.archura.platform.agent;
 
-import javassist.CannotCompileException;
 import javassist.ClassPool;
 import javassist.CtClass;
 import javassist.CtConstructor;
-import javassist.CtMethod;
 
 import java.io.ByteArrayInputStream;
 import java.lang.instrument.ClassFileTransformer;
 import java.lang.instrument.IllegalClassFormatException;
 import java.security.ProtectionDomain;
-import java.util.logging.Logger;
 
 public class FilterFunctionTransformer implements ClassFileTransformer {
 
-    private final Logger log = Logger.getLogger(FilterFunctionTransformer.class.getSimpleName());
     private final String executorClassName;
 
     public FilterFunctionTransformer(final String className) {
@@ -24,48 +20,12 @@ public class FilterFunctionTransformer implements ClassFileTransformer {
     public byte[] transform(ClassLoader loader, String className, Class classBeingRedefined,
                             ProtectionDomain protectionDomain, byte[] classfileBuffer) throws IllegalClassFormatException {
         byte[] byteCode = classfileBuffer;
-        byteCode = handleThreadClass(className, classfileBuffer, byteCode);
         byteCode = handleSocketClass(className, classfileBuffer, byteCode);
         return byteCode;
     }
 
-    private byte[] handleThreadClass(String className, byte[] classfileBuffer, byte[] byteCode) {
-        if (className.contains("java/lang/Thread")) {
-            try {
-                ClassPool classPool = ClassPool.getDefault();
-                CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
-                CtMethod[] methods = ctClass.getDeclaredMethods();
-                for (CtMethod method : methods) {
-                    if (method.getName().contains("start")
-                            || method.getName().contains("next")) {
-                        try {
-                            method.insertBefore(getAllowCheckCode());
-                        } catch (CannotCompileException error) {
-                            if ("no method body".equalsIgnoreCase(error.getMessage())) {
-                                log.warning("Could not compile and insert code, 'no method body' in the method: " + method.getLongName());
-                            } else {
-                                log.severe("Could not compile and insert code, method: " + method.getLongName() + " error: " + error.getMessage());
-                            }
-                        } catch (Throwable throwable) {
-                            log.severe("Error occurred while compiling and inserting code, method: " + method.getLongName() + " error: " + throwable.getMessage());
-                            throwable.printStackTrace();
-                        }
-                    }
-                }
-                byteCode = ctClass.toBytecode();
-                ctClass.detach();
-            } catch (Throwable throwable) {
-                System.out.println("throwable.getMessage() = " + throwable.getMessage());
-            }
-        }
-        return byteCode;
-    }
-
     private byte[] handleSocketClass(String className, byte[] classfileBuffer, byte[] byteCode) {
-        if ((className.startsWith("java/net/") && className.contains("Socket"))
-                || className.contains("java/util/concurrent/AbstractExecutorService")
-                || className.contains("java/lang/Thread")
-        ) {
+        if (className.startsWith("java/net/") && className.contains("Socket")) {
             try {
                 ClassPool classPool = ClassPool.getDefault();
                 CtClass ctClass = classPool.makeClass(new ByteArrayInputStream(classfileBuffer));
@@ -84,13 +44,18 @@ public class FilterFunctionTransformer implements ClassFileTransformer {
 
     private String getAllowCheckCode() {
         return "                     for (int i=0; i< Thread.currentThread().getStackTrace().length; i++) {\n" +
-                "                        final String className = Thread.currentThread().getStackTrace()[i].getClassName();\n" +
+                "                        StackTraceElement element = Thread.currentThread().getStackTrace()[i];\n" +
+                "                        final String className = element.getClassName();\n" +
+                "                        final String methodName = element.getMethodName();\n" +
                 "                        if(className.contains(\"jdk.internal.net.http.HttpClientImpl\")) {\n" +
+                "                            //System.out.println(\">>>>> EXPLICITLY ALLOWED className = \" + className + \" \" + Thread.currentThread().getName() + \" \" + methodName);\n" +
                 "                            break;\n" +
                 "                        }\n" +
                 "                        if(className.contains(\"" + executorClassName + "\")){\n" +
-                "                            throw new RuntimeException(\"Filters/Functions are not allowed to create Sockets/Threads.\");\n" +
+                "                            //System.out.println(\">>>>> DENIED className = \" + className + \" \" + Thread.currentThread().getName() + \" \" + methodName);\n" +
+                "                            throw new RuntimeException(\"Filters/Functions are not allowed to create Sockets.\");\n" +
                 "                        }\n" +
+                "                        //System.out.println(\">>>>> IMPLICITLY ALLOWED className = \" + className + \":\" + methodName + \" [\" + Thread.currentThread().getName() + \"]  this.getClass().getName(): \" + this.getClass().getName());\n" +
                 "                    };";
     }
 
